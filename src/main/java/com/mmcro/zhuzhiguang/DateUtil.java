9f02783a897844b3413956ea9ec7407bdc0af783
moteinstorm <454665830@qq.com>
2019-09-06 14:15:00 +0800

    增加计算年龄的方法

--- src/main/java/com/mmcro/zhuzhiguang/DateUtil.java
+++ src/main/java/com/mmcro/zhuzhiguang/DateUtil.java
@@ -1,10 +1,46 @@
 package com.mmcro.zhuzhiguang;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * 
  * @author zhuzg
  *
  */
 public class DateUtil {
-	//
+	
+	/**
+	 * 
+	 * @param birthday
+	 * @return
+	 */
+	public int getAge(Date birthday) {
+		
+		Calendar instance = Calendar.getInstance();
+		instance.setTime(birthday);
+		
+		//得到出生的年
+		int birthYear = instance.get(Calendar.YEAR);
+		int birthMonth = instance.get(Calendar.MONTH);
+		int birthDate = instance.get(Calendar.DAY_OF_MONTH);
+		
+		//获取当前的年月日
+		instance.setTime(new Date());
+		int currentYear = instance.get(Calendar.YEAR);
+		int currentMonth = instance.get(Calendar.MONTH);
+		int currentDate = instance.get(Calendar.DAY_OF_MONTH);
+		
+		int age = currentYear-birthYear;
+		if(currentMonth<birthMonth) {
+			age--;
+		}else if(currentMonth==birthMonth && currentDate<birthDate){
+			age --;
+		}
+		
+		return age;
+		
+	}
+	
+	
 }
